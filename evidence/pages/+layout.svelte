12e7bf2a175aa769f6d2d570fb5e47fe5772c059
Yoshinori Teraoka <teraoka@3-shake.com>
2024-03-14 18:32:11 +0900

    evidence の更新と full width 化

---
+++ evidence/pages/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import '@evidence-dev/tailwind/fonts.css';
+    import '../app.css';
+    import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+    export let data;
+</script>
+
+<EvidenceDefaultLayout title="My Title" fullWidth=true hideSidebar=true githubRepo="https://github.com/yteraoka/amazon-price-watcher" {data}>
+    <slot slot="content" />
+</EvidenceDefaultLayout>
